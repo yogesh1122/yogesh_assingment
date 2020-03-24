eeb5decc20e852619125735aad466504525c83c4
Yogesh <53933129+yogesh1122@users.noreply.github.com>
2020-03-24 16:26:48 +0530

    ook

--- routes/user.js
+++ routes/user.js
@@ -5,8 +5,5 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/getReports', userController.getReports );
-
-
-
-//check it git 
+ 
 module.exports = router;
