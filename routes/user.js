19fbb620d880fc2859f01330f4329a657efb765c
Yogesh <53933129+yogesh1122@users.noreply.github.com>
2020-03-24 16:38:29 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -6,9 +6,14 @@ const userController = require('../controllers/userController');
 
 router.get('/getReports', userController.getReports );
  
+<<<<<<< HEAD
 module.exports = router;
 
 //here are router codes
 
 
+=======
+
+//hello
 module.exports = router;
+>>>>>>> 1233dcf3b495a17822c7a51b9054d211a17313a1
